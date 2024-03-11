7ae90afe959aaab78b327e40f4b886770a286288
cephonodes <yuki_cephonodes@outlook.com>
2024-03-12 08:12:35 +0900

    テーブルにインデックスを追加した

--- postgres/init/01_create_table.sql
+++ postgres/init/01_create_table.sql
@@ -2,12 +2,14 @@ create table facilitators (
     id serial not null,
     primary key (id)
 );
+create index facilitators_id_index on facilitators (id);
 
 create table classrooms (
         id serial not null,
         name varchar(50) not null,
         primary key (id)
 );
+create index classrooms_id_index on classrooms (id);
 
 create table students (
     id serial not null,
@@ -17,6 +19,8 @@ create table students (
     primary key (id),
     foreign key (classroom_id) references classrooms(id)
 );
+create index students_name_index on students (name);
+create unique index students_login_id_index on students (login_id);
 
 create table facilitator_classroom_relation (
     id serial not null,
@@ -26,3 +30,4 @@ create table facilitator_classroom_relation (
     foreign key (facilitator_id) references facilitators(id),
     foreign key (classroom_id) references classrooms(id)
 );
+create unique index facilitator_classroom_relation_foreign_keys_index on facilitator_classroom_relation (facilitator_id, classroom_id);
